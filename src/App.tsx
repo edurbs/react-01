86d0efa24c3a92b1549a2b3ef5fc77b9e76ee5e6
Eduardo Soares <edurbs@gmail.com>
2023-11-15 17:01:10 -0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card } from './components/Card';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout>
+      Hello world
+      <>
+        <Card 
+          id={1} 
+          paragraph='Typescript'
+          details='front e back'
+        />
+      </>
+      <>
+      <div>
+        <Card 
+          id={2}
+          paragraph='HTML'
+          details='front'
+        />
+      </div>
+      </>
+      <>
+      <div>
+        <Card 
+          id={2}
+          paragraph='SQL'
+          details='banco de dados'
+          />
+      </div>
+      </> 
+      </Layout>
+    </>
   );
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return(
+    <div>
+      Aqui vai o rodapé
+    </div>
+  )
+}
